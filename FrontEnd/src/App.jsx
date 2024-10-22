02ecde0aab5d736498118907c18db8e3e872a21e
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-10-22 08:30:07 -0600

    Revert "ADD and SELECT"
    This reverts commit 98ec220e7aa9e197b8672754f7129b214d2ef9ec.

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,20 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import TableComponent from './Component/TableComponent';
-import AddUser from './Component/AddUser';
-import UpdatedUser from './Component/UpdatedUser';
-
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<TableComponent />} />
-      <Route path="/User" element={<AddUser />} />
-      <Route path="/Update/:id" element={<UpdatedUser />} />
-    </Routes>
-  </BrowserRouter>
+    <div>
+      <h1>Holasdfa</h1>
+    </div>
   )
 }
 
